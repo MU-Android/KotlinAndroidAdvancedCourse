6d0ffb4814fcf24f72d9b83cec93a7ba18222c41
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-18 18:35:10 +0300

    feat(upload): add image selection from gallery with permissions
    * Implemented image selection functionality using ActivityResultLauncher
    * Added permission handling for reading media images and external storage

--- 1-Firebase/app/src/main/java/com/musauyumaz/sharephoto/UploadFragment.kt
+++ 1-Firebase/app/src/main/java/com/musauyumaz/sharephoto/UploadFragment.kt
@@ -1,12 +1,23 @@
 package com.musauyumaz.sharephoto
 
 import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log.e
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -16,8 +27,13 @@ import com.musauyumaz.sharephoto.databinding.FragmentUploadBinding
 class UploadFragment : Fragment() {
     private var _binding : FragmentUploadBinding? = null
     private val binding get() = _binding!!
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture: Uri? = null
+    var selectedBitmap: Bitmap?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        registerLaunchers()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,11 +51,28 @@ class UploadFragment : Fragment() {
             if (ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_MEDIA_IMAGES)) {
                     Snackbar.make(view,"Galeriye gitmek için izin vermeniz gerekiyor",Snackbar.LENGTH_INDEFINITE).setAction("İzin Ver", View.OnClickListener{
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
                     }).show()
+                }else{
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
                 }
+            }else{
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
             }
         } else {
-
+            if (ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Snackbar.make(view,"Galeriye gitmek için izin vermeniz gerekiyor",Snackbar.LENGTH_INDEFINITE).setAction("İzin Ver", View.OnClickListener{
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }).show()
+                }else{
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }else{
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }
         }
     }
 
@@ -55,5 +88,37 @@ class UploadFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun registerLaunchers(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null) {
+                    selectedPicture = intentFromResult.data
+                    try {
+                        if(Build.VERSION.SDK_INT >= 28){
+                            val source = ImageDecoder.createSource(requireActivity().contentResolver,selectedPicture!!)
+                            selectedBitmap = ImageDecoder.decodeBitmap(source)
+                            binding.imageView.setImageBitmap(selectedBitmap)
+                        }else{
+                            selectedBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,selectedPicture)
+                            binding.imageView.setImageBitmap(selectedBitmap)
+                        }
+                    }catch (e: Exception){
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){result ->
+            if(result){
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                Toast.makeText(requireContext(),"İzin reddedildi izne ihtiyacımız var",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
