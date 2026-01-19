d55fea425be42fe863c63766831510a4156ee5dc
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-19 22:48:30 +0300

    feat(feed): sort posts by date in descending order
    * Implemented ordering of posts in the Firestore query
    * Enhanced user experience by displaying the most recent posts first

--- 1-Firebase/app/src/main/java/com/musauyumaz/sharephoto/view/FeedFragment.kt
+++ 1-Firebase/app/src/main/java/com/musauyumaz/sharephoto/view/FeedFragment.kt
@@ -14,6 +14,7 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 import com.musauyumaz.sharephoto.FeedFragmentDirections
 import com.musauyumaz.sharephoto.R
@@ -83,7 +84,7 @@ class FeedFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     }
 
     private fun fireStoreGetData(){
-        db.collection("Posts").addSnapshotListener { value, error ->
+        db.collection("Posts").orderBy("date", Query.Direction.DESCENDING).addSnapshotListener { value, error ->
             if (error != null){
                 Toast.makeText(requireContext(),error.localizedMessage,Toast.LENGTH_LONG).show()
             }else{
