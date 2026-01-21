01e8af8c9171747af86534935f3f1d26ffa9b422
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-21 22:48:19 +0300

    feat(navigation): add navigation graph and update layouts
    * Introduced a navigation graph for fragment navigation
    * Updated main activity layout to include a FragmentContainerView

--- 2-RetrofitAndMVVM/NutritionBook/app/build.gradle.kts
+++ 2-RetrofitAndMVVM/NutritionBook/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
