ecf0086ca60dcea8a686871343f523e079e07e31
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-22 18:35:12 +0300

    feat(nutrition): implement view binding in fragments
    * Added view binding to NutritionDetailFragment for improved UI handling
    * Integrated view binding in NutritionListFragment to enhance layout management

--- 2-RetrofitAndMVVM/NutritionBook/app/src/main/java/com/musauyumaz/nutritionbook/NutritionDetailFragment.kt
+++ 2-RetrofitAndMVVM/NutritionBook/app/src/main/java/com/musauyumaz/nutritionbook/NutritionDetailFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.musauyumaz.nutritionbook.databinding.FragmentNutritionDetailBinding
 
 class NutritionDetailFragment : Fragment() {
+    private var _binding: FragmentNutritionDetailBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,6 +18,16 @@ class NutritionDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_nutrition_detail, container, false)
+        _binding = FragmentNutritionDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- 2-RetrofitAndMVVM/NutritionBook/app/src/main/java/com/musauyumaz/nutritionbook/NutritionListFragment.kt
+++ 2-RetrofitAndMVVM/NutritionBook/app/src/main/java/com/musauyumaz/nutritionbook/NutritionListFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.musauyumaz.nutritionbook.databinding.FragmentNutritionListBinding
 
 class NutritionListFragment : Fragment() {
+    private var _binding: FragmentNutritionListBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,6 +18,16 @@ class NutritionListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_nutrition_list, container, false)
+        _binding = FragmentNutritionListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
