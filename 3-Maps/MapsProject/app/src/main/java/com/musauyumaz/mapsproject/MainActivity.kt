ad6fd7ff3c58efc760c202f46f6189aea85dfdd0
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2026-01-24 09:18:13 +0300

    feat(maps): integrate OSM library and implement map view
    * Added OSM library to dependencies for map functionality
    * Implemented map view in MainActivity with location permissions

--- 3-Maps/MapsProject/app/src/main/java/com/musauyumaz/mapsproject/MainActivity.kt
+++ 3-Maps/MapsProject/app/src/main/java/com/musauyumaz/mapsproject/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Configuration.getInstance().userAgentValue = packageName
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        mapView = findViewById(R.id.mapView)
+        mapView.setMultiTouchControls(true)
+
+        val startPoint = GeoPoint(41.015137, 28.979530)
+        mapView.controller.setZoom(13.0)
+        mapView.controller.setCenter(startPoint)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
     }
 }
